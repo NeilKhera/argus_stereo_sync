ecb66d27214094ff3dce0a7523bd94abdae21057
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2020-02-22 00:04:06 -0500

    Works. Added Testing code. Did 3 tests.

--- main.cpp
+++ main.cpp
@@ -10,15 +10,23 @@
 #include "ArgusHelpers.h"
 #include "CommonOptions.h"
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <fstream>
+
 using namespace Argus;
 using namespace EGLStream;
+
+std::ofstream outfile;
 
 namespace ArgusSamples {
 
 EGLDisplayHolder g_display;
 
-static const Size2D<uint32_t> STREAM_SIZE(960, 540);
-static const uint32_t CAPTURE_TIME = 60;
+static const Size2D<uint32_t> STREAM_SIZE(1280, 720);
+static const uint32_t FRAMERATE = 5;
 
 #define PRODUCER_PRINT(...) printf("PRODUCER: " __VA_ARGS__)
 #define CONSUMER_PRINT(...) printf("CONSUMER: " __VA_ARGS__)
@@ -50,62 +58,82 @@ bool StereoConsumerThread::threadInitialize() {
     return true;
 }
 
-bool StereoConsumerThread::threadExecute() {
+bool StereoConsumerThread::threadExecute() { 
     IEGLOutputStream *leftIStream = interface_cast<IEGLOutputStream>(m_leftStream);
     IEGLOutputStream *rightIStream = interface_cast<IEGLOutputStream>(m_rightStream);
     IFrameConsumer *leftIFrameConsumer = interface_cast<IFrameConsumer>(m_leftConsumer);
     IFrameConsumer *rightIFrameConsumer = interface_cast<IFrameConsumer>(m_rightConsumer);
-    //Argus::Status status_left;
-    //Argus::Status status_right;
 
     CONSUMER_PRINT("Waiting until producer is connected...\n");
     if (leftIStream->waitUntilConnected() != STATUS_OK || rightIStream->waitUntilConnected() != STATUS_OK) {
         ORIGINATE_ERROR("Stream failed to connect.");
     }
-    CONSUMER_PRINT("Producer has connected; continuing.\n");
+    CONSUMER_PRINT("Streams connected, processing frames.\n");
 
     int frameCount = 0;
     while (true) {
+	EGLint streamState = EGL_STREAM_STATE_CONNECTING_KHR;
+	if (!eglQueryStreamKHR(
+		leftIStream->getEGLDisplay(), 
+		leftIStream->getEGLStream(),
+		EGL_STREAM_STATE_KHR,
+		&streamState) || (streamState == EGL_STREAM_STATE_DISCONNECTED_KHR)) {
+	    CONSUMER_PRINT("EGL_STREAM_STATE_DISCONNECTED_KHR received\n");
+	    break;
+	}
+
+        if (!eglQueryStreamKHR(
+                rightIStream->getEGLDisplay(),
+                rightIStream->getEGLStream(),
+                EGL_STREAM_STATE_KHR,
+                &streamState) || (streamState == EGL_STREAM_STATE_DISCONNECTED_KHR)) {
+            CONSUMER_PRINT("EGL_STREAM_STATE_DISCONNECTED_KHR received\n");
+            break;
+        }
+	//std::cout << "Boop" << std::endl;
+
 	UniqueObj<Frame> left_frame(leftIFrameConsumer->acquireFrame());
+	auto frame1_timestamp = std::chrono::system_clock::now();
         UniqueObj<Frame> right_frame(rightIFrameConsumer->acquireFrame());
-        IFrame *left_iframe = interface_cast<IFrame>(left_frame);
-        IFrame *right_iframe = interface_cast<IFrame>(right_frame);
+	auto frame2_timestamp = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> diff = frame1_timestamp - frame2_timestamp;
+	outfile << -diff.count() << "\n";
+
+        if (!left_frame || !right_frame) {
+	    break;
+	}
+
+	IFrame *left_iframe = interface_cast<IFrame>(left_frame);
+	IFrame *right_iframe = interface_cast<IFrame>(right_frame);
+	if (!left_iframe || !right_iframe) {
+	    break;
+	}
 
         Image *left_image = left_iframe->getImage();
         Image *right_image = right_iframe->getImage();
-	
-	/*IImage *left_iimage = interface_cast<IImage>(left_image);
-	IImage *right_iimage = interface_cast<IImage>(right_image);
-	IImage2D *left_iimage2d = interface_cast<IImage2D>(left_image);
-	IImage2D *right_iimage2d = interface_cast<IImage2D>(right_image);
         
-	cv::Mat imgbuf_left = cv::Mat(cv::Size(STREAM_SIZE.width(), STREAM_SIZE.height()), CV_8UC1, (void*)left_iimage->mapBuffer(), (size_t)left_iimage2d->getStride());
-	cv::Mat imgbuf_right = cv::Mat(cv::Size(STREAM_SIZE.width(), STREAM_SIZE.height()), CV_8UC1, (void*)right_iimage->mapBuffer(), (size_t)right_iimage2d->getStride());
-	cv::imwrite("/home/lunarleopard2/Desktop/left.jpg", imgbuf_left);
-	cv::imwrite("/home/lunarleopard2/Desktop/right.jpg", imgbuf_right);
-	//cv::imshow("img_left", imgbuf_left);
-        //cv::imshow("img_right", imgbuf_right);
-        cv::waitKey(1);*/
-
-        IImageJPEG *iJPEG_left = interface_cast<IImageJPEG>(left_image);
+	IImageJPEG *iJPEG_left = interface_cast<IImageJPEG>(left_image);
 	IImageJPEG *iJPEG_right = interface_cast<IImageJPEG>(right_image);
-        if (!iJPEG_left || !iJPEG_right)
+        if (!iJPEG_left || !iJPEG_right) {
             ORIGINATE_ERROR("Failed to get IImageJPEG interface.");
+	}
 
         std::ostringstream fileName1;
 	std::ostringstream fileName2;
         fileName1 << "left_" << std::setfill('0') << std::setw(4) << frameCount << ".jpg";
-	fileName2 << "right_" << std::setfill('0') << std::setw(4) << frameCount++ << ".jpg";
-        if (iJPEG_left->writeJPEG(fileName1.str().c_str()) == STATUS_OK)
-            CONSUMER_PRINT("Captured a still image to '%s'\n", fileName1.str().c_str());
-        else
+	fileName2 << "right_" << std::setfill('0') << std::setw(4) << frameCount << ".jpg";
+        if (iJPEG_left->writeJPEG(fileName1.str().c_str()) == STATUS_OK) {
+            //CONSUMER_PRINT("Captured a still image to '%s'\n", fileName1.str().c_str());
+    	} else {
             ORIGINATE_ERROR("Failed to write JPEG to '%s'\n", fileName1.str().c_str());
+	}
 
-        if (iJPEG_right->writeJPEG(fileName2.str().c_str()) == STATUS_OK)
-            CONSUMER_PRINT("Captured a still image to '%s'\n", fileName2.str().c_str());
-        else
+        if (iJPEG_right->writeJPEG(fileName2.str().c_str()) == STATUS_OK) {
+            //CONSUMER_PRINT("Captured a still image to '%s'\n", fileName2.str().c_str());
+	} else {
             ORIGINATE_ERROR("Failed to write JPEG to '%s'\n", fileName2.str().c_str());
-
+	}
     }
     
     CONSUMER_PRINT("No more frames. Cleaning up.\n");
@@ -146,7 +174,7 @@ static bool execute() {
     UniqueObj<OutputStreamSettings> streamSettings(iCaptureSession->createOutputStreamSettings(STREAM_TYPE_EGL));
     IOutputStreamSettings *iStreamSettings = interface_cast<IOutputStreamSettings>(streamSettings);
     IEGLOutputStreamSettings *iEGLStreamSettings = interface_cast<IEGLOutputStreamSettings>(streamSettings);
-    if (!iEGLStreamSettings) {
+    if (!iStreamSettings || !iEGLStreamSettings) {
         ORIGINATE_ERROR("Failed to create OutputStreamSettings");
     }
     iEGLStreamSettings->setPixelFormat(PIXEL_FMT_YCbCr_420_888);
@@ -157,14 +185,16 @@ static bool execute() {
     PRODUCER_PRINT("Creating left stream.\n");
     iStreamSettings->setCameraDevice(lrCameras[0]);
     UniqueObj<OutputStream> streamLeft(iCaptureSession->createOutputStream(streamSettings.get()));
-    if (!streamLeft.get()) {
+    IEGLOutputStream *iStreamLeft = interface_cast<IEGLOutputStream>(streamLeft);
+    if (!iStreamLeft) {
         ORIGINATE_ERROR("Failed to create left stream");
     }
 
     PRODUCER_PRINT("Creating right stream.\n");
     iStreamSettings->setCameraDevice(lrCameras[1]);
     UniqueObj<OutputStream> streamRight(iCaptureSession->createOutputStream(streamSettings.get()));
-    if (!streamRight.get()) {
+    IEGLOutputStream *iStreamRight = interface_cast<IEGLOutputStream>(streamRight);
+    if (!iStreamRight) {
         ORIGINATE_ERROR("Failed to create right stream");
     }
 
@@ -178,34 +208,41 @@ static bool execute() {
     if (!iRequest) {
         ORIGINATE_ERROR("Failed to create Request");
     }
+
+    ISourceSettings *iSourceSettings = interface_cast<ISourceSettings>(iRequest->getSourceSettings());
+    iSourceSettings->setFrameDurationRange(Range<uint64_t>(1e9/FRAMERATE));
+
     iRequest->enableOutputStream(streamLeft.get());
     iRequest->enableOutputStream(streamRight.get());
 
     PRODUCER_PRINT("Starting repeat capture requests.\n");
-    for (uint32_t i = 0; i < CAPTURE_TIME; i++) {
-    	if (iCaptureSession->capture(request.get()) == 0) {
-            ORIGINATE_ERROR("Failed to start repeat capture request for preview");
-    	}
-	sleep(1);
+    if (iCaptureSession->repeat(request.get()) != STATUS_OK) {
+	ORIGINATE_ERROR("Failed to start repeat capture request for preview");
     }
+    sleep(3600);
+
     iCaptureSession->stopRepeat();
     iCaptureSession->waitForIdle();
 
     PRODUCER_PRINT("Captures complete, disconnecting producer.\n");
-    streamLeft.reset();
-    streamRight.reset();
+    iStreamLeft->disconnect();
+    iStreamRight->disconnect();
+
     PROPAGATE_ERROR(stereoConsumer.shutdown());
     cameraProvider.reset();
+    PROPAGATE_ERROR(g_display.cleanup());
 
     PRODUCER_PRINT("Done -- exiting.\n");
-    return 0;
+    return true;
 }
 
 };
 
 int main (int argc, char *argv[]) {
+    outfile.open("test3.txt");
     if (!ArgusSamples::execute()) {
         return EXIT_FAILURE;
     }
+    outfile.close();
     return EXIT_SUCCESS;
 }
